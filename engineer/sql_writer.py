03fc4c3bb4e031f75e6f8f2d2843c90bd4aba15f
bezdomnij <attlila.novak@yahoo.co.uk>
2021-12-28 08:52:35 +0100

    refactored checker functions

--- engineer/sql_writer.py
+++ engineer/sql_writer.py
@@ -5,8 +5,8 @@ import sys
 
 
 def write_to_db(df, table_name, action='replace', hova='19'):
-    engine = get_engine(hova, db_name='stage')
-    connection = engine.connect()
+    sql_engine = get_engine(hova, db_name='stage')
+    connection = sql_engine.connect()
     types = get_types(df, milyen='mindegy')
     try:
         df.to_sql(table_name, connection, if_exists=action, index=False,
@@ -22,7 +22,6 @@ def write_to_db(df, table_name, action='replace', hova='19'):
 def get_engine(which_one, db_name='stage'):
     if which_one == '19':
         username = os.getenv('U_19')
-        # breakpoint()
         pw = os.getenv('P_19')
         server = '192.168.2.19'
     elif which_one == 'pd':
@@ -33,10 +32,9 @@ def get_engine(which_one, db_name='stage'):
         print("No destination SERVER given! Exiting")
         sys.exit(1)
     connection_uri = f'mysql+pymysql://{username}:{pw}@{server}:3306/{db_name}?charset=utf8mb4'
-    print(connection_uri)
     connect_args = {'init_command': "SET @@collation_connection='utf8mb4_hungarian_ci'",
                     'read_timeout': 30}
-    sql_engine = create_engine(connection_uri, pool_pre_ping=True, pool_recycle=3600, echo=True,
+    sql_engine = create_engine(connection_uri, pool_pre_ping=True, pool_recycle=3600, echo=False,
                                echo_pool=True, connect_args=connect_args, future=False)
     return sql_engine
 
--- libreka/libreka.py
+++ libreka/libreka.py
@@ -15,56 +15,69 @@ def get_comparable_names(columns):
 
 
 def check_sheet_df(sheet, df):
+    filename = 'sheet_fields_libreka.txt'
+    file_path = Path.cwd() / 'libreka' / filename
     error = 0
-    with open('sheet_field.txt', mode='r') as f:
-        lines = f.readlines()
-        sheets = [line.strip().split(';')[0] for line in lines]
-        fields = [line.strip().split(';')[1] for line in lines]
-        for index, s in enumerate(sheets):
-            if s == sheet:
-                comparable_field_names = get_comparable_names(list(df.columns))
-                if not (fields[index].split(',') == comparable_field_names):
-                    error = 1
-                    break
+    try:
+        with open(file_path, mode='r') as f:  # read list of expected column names
+            lines = f.readlines()
+            sheets = [line.strip().split(';')[0] for line in lines]
+            fields = [line.strip().split(';')[1] for line in lines]
+            for index, s in enumerate(sheets):
+                if s == sheet:
+                    df_field_names = get_comparable_names(list(df.columns))
+                    if not (fields[index].split(',') == df_field_names):  # compare two lists
+                        error = 1
+                        break
+    except FileNotFoundError as fnfe:
+        print(f"but CAN'T find field-names file: {filename} to check fields in sheet:\n error {fnfe}")
+        error = 2
     return error
 
 
-def check_file_content(f):
-    error = False
-    sheet_name_originals = ('E-Book-Verkäufe', 'Hörbuch-Verkäufe', 'Kostenlostitel', 'Abo und Flatrate')
-    tables = ('e_book', 'a_book', 'free', 'sub')
-    good_names = dict(zip(sheet_name_originals, tables))
+def check_file_sheet_names(f, sheet_name_originals):
     try:
         sheets_in_file = pd.read_excel(f, sheet_name=None, header=0)
         for sheet in sheets_in_file:
             if sheet not in sheet_name_originals:
-                print(f'Alert, unknown sheet in {f}, this one: {sheet}')
-                error = True
-                break
-        if not error:
-            print(f'Sheets are as expected in file {f}')
-            for sheet, df in sheets_in_file.items():
-                table_name = 'libreka_' + good_names[sheet]
-                field_error = check_sheet_df(sheet, df)
-                if field_error:
-                    print(f"\nERROR!!! \nColumns in file `{f.name}`, sheet `{sheet}` NOT matching the expected.")
-                    print('not going to write it to db. Signing off...')
-                    sys.exit(1)
-                print(table_name)
-                sql_writer.write_to_db(df, table_name, 'append', '19')
-        else:
-            print(f"There's some weird shit going on in {f}, unknown sheet in file.")
-    except ValueError as e:
-        print(f'File is open OR not Excel, error: {e}')
-    finally:
-        print(f'operation on file {f} is finished')
+                print(f'Alert, unknown sheet {sheet} in {f}: ')
+                return 1
+        return 0, sheets_in_file
+    except ValueError as ve:
+        print(f'File is open, unreadable, error: {ve}')
+        return 2
 
 
 def main(dirpath):
     p = Path(dirpath)
     excel_files = [item for item in p.iterdir() if item.is_file() and item.suffix == '.xlsx']
+    sheet_name_originals = ('E-Book-Verkäufe', 'Hörbuch-Verkäufe', 'Kostenlostitel', 'Abo und Flatrate')
+    tables = ('e_book', 'a_book', 'free', 'sub')
+    good_for_table_names = dict(zip(sheet_name_originals, tables))
+
     for f in excel_files:
-        check_file_content(f)
+        sheet_error, sheets_in_file = check_file_sheet_names(f, sheet_name_originals)
+        if sheet_error == 0:
+            print(f'Sheets are as expected in file {f}')
+        elif sheet_error == 1:
+            print(f"There's some weird shit going on in {f}, unknown sheet in file.")
+            print('Signing off...')
+            sys.exit(1)
+        elif sheet_error == 2:
+            print('File read error, signing off...')
+            sys.exit(1)
+        for sheet, df in sheets_in_file.items():
+            field_error = check_sheet_df(sheet, df)  # check fields in sheet
+            if field_error == 1:
+                print(f"ERROR!!! \nColumns in file `{f.name}`, sheet `{sheet}` NOT matching the expected.")
+                print('not going to write any further to db. Clean up db!\nSigning off...')
+                sys.exit(1)
+            elif field_error == 2:
+                print('Checking field names failed, no Libreka field reference found, signing off...')
+                sys.exit(2)
+            else:
+                table_name = 'libreka_' + good_for_table_names[sheet]
+                sql_writer.write_to_db(df, table_name, 'append', '19')  # select DB here
 
 
 if __name__ == '__main__':
