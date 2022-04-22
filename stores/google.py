7982a762ef62c44cc9e5132d7b0c7e071dfb3e17
bezdomnij <bajni37@gmail.com>
2022-04-22 23:08:22 +0200

    google audio and main modified for szamitas

--- stores/google.py
+++ stores/google.py
@@ -1,43 +1,42 @@
 import logging
 from pathlib import Path
-
+from config import MAIN_DIR, REPORT_MONTH
 import pandas as pd
-from util import util
+import util
 from engineer import sql_writer as sqw
 
 DATA_DIR = 'google'
-SOURCE_DIR = '2022_02_february'
 TABLE = 'stg_fin2_12_googleplay'
 
 
-def google(finrep_dir, hova='0'):
-    files = []
+def google(hova='0'):
     df = pd.DataFrame()
-    src = Path(finrep_dir).joinpath(SOURCE_DIR).joinpath(DATA_DIR)
-    for f in src.iterdir():
-        if f.suffix == '':
-            continue
-        ftype, period = util.check_google_file_name(f.name)
-        year, month = period
-        if ftype == 'gg' and int(month) == 2:
-            files.append(f)
-            logging.info(msg=f'file found:{f}')
-    for f in files:
-        try:
-            df = pd.read_csv(f, encoding='utf-8', sep='\t', header=0, index_col=None)
-        except Exception as e:
-            logging.exception(msg=f'error!!! {e}')
-            # print(f"az ebooks file nem utf-16..., error: {e}")
-            df = pd.read_csv(f, encoding='utf-16', sep='\t', header=0, index_col=None)
-        finally:
-            # print(df.columns)
-            # print(df.info)
-            df['Earnings Amount'] = df['Earnings Amount'].str.replace(',', '.')
-            print(f"{f.name} SUM: {df['Earnings Amount'].astype(float).sum():.2f}, row count: {df.shape[0]}")
-    sqw.write_to_db(df, TABLE, action='replace', hova=hova, field_lens='vchall')
+    src = Path(MAIN_DIR).joinpath(REPORT_MONTH).joinpath(DATA_DIR)
+    files = util.get_file_list(src)
+    if files:
+        for f in src.iterdir():
+            if f.suffix == '':
+                continue
+            ftype, period = util.check_google_file_name(f.name)
+            year, month = period
+            if ftype == 'gg' and int(month) == 2:
+                files.append(f)
+                logging.info(msg=f'file found:{f}')
+        for f in files:
+            try:
+                df = pd.read_csv(f, encoding='utf-8', sep='\t', header=0, index_col=None)
+            except Exception as e:
+                logging.exception(msg=f'error!!! {e}')
+                # print(f"az ebooks file nem utf-16..., error: {e}")
+                df = pd.read_csv(f, encoding='utf-16', sep='\t', header=0, index_col=None)
+            finally:
+                # print(df.columns)
+                # print(df.info)
+                df['Earnings Amount'] = df['Earnings Amount'].str.replace(',', '.')
+                print(f"{f.name} SUM: {df['Earnings Amount'].astype(float).sum():.2f}, row count: {df.shape[0]}")
+        sqw.write_to_db(df, TABLE, action='replace', hova=hova, field_lens='vchall')
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, filename='datacamp.log', filemode='w', format='%(asctime)s %(message)s')
-    # google('/Users/frank/pd/finance_report/2022_01_january', hova='0')
-    google('h:/NextCloud/Finance/szamitas', hova='0')
+    google(hova='19')
--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,4 @@
 from .util import get_proper_df
 from .util import get_content_xl_onesheet
 from .util import get_file_list
+from .util import check_google_file_name
