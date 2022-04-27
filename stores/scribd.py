3cd2b03bd9d4ca6b439080c5cc999d8b51edcd6b
bezdomnij <attlila.novak@yahoo.co.uk>
2022-04-27 22:48:01 +0200

    refactored scribd for march report

--- stores/scribd.py
+++ stores/scribd.py
@@ -1,43 +1,46 @@
 from pathlib import Path
 import warnings
 import pandas as pd
+
+import util
 from engineer import sql_writer as sqw
+from config import MAIN_DIR, REPORT_MONTH
 
 FILENAME_1 = 'PublishDrive_scribd_subscriptions_payouts_'
 FILENAME_2 = 'PublishDrive2_scribd_subscriptions_payouts_'
 TABLE = 'stg_fin2_19_scribd'
-SOURCE_DIR = '2022_02_february'
-REPORT_MONTH = '2022_02_february'
 DATA_DIR = 'scribd'
 SUM_FIELD = 'Amount owed for this interaction'
 
 
-def scribd(dirpath, hova='0'):
+def scribd(hova='0'):
     all_df = pd.DataFrame()
+    p = Path(MAIN_DIR).joinpath(REPORT_MONTH).joinpath(DATA_DIR)
+    files = util.get_file_list(p)
     szumma = 0
-    p = Path(dirpath).joinpath(SOURCE_DIR).joinpath(DATA_DIR)
     record_count = 0
-    for f in p.iterdir():
-        if f.is_file() and (f.suffix == '.xlsx' or f.suffix == '.xls' or f.suffix == '.XLS') and (
-                FILENAME_1 in f.stem or FILENAME_2 in f.stem) and f.stem[:2] != '~$':
-            with warnings.catch_warnings(record=True):
-                warnings.simplefilter("always")
-                df = pd.read_excel(f, header=0, index_col=None)
-            if df.shape[0] > 0:
-                record_count += df.shape[0]
-                szumma += df[SUM_FIELD].sum()
-                all_df = all_df.append(df)
-                print(f'file: {f.stem}, {df.shape[0]} records, {szumma:-10,.3f}')
+    if files:
+        for f in files:
+            if f.is_file() and (f.suffix == '.xlsx' or f.suffix == '.xls' or f.suffix == '.XLS') and (
+                    FILENAME_1 in f.stem or FILENAME_2 in f.stem) and f.stem[:2] != '~$':
+                with warnings.catch_warnings(record=True):
+                    warnings.simplefilter("always")
+                    df = pd.read_excel(f, header=0, index_col=None)
+                if df.shape[0] > 0:
+                    rc = df.shape[0]
+                    szm = df[SUM_FIELD].sum()
+                    all_df = all_df.append(df)
+                    print(f'file: {f.stem}, {rc:10d} records, {szm:10,.3f}')
+                    record_count += rc
+                    szumma += szm
 
-    print(f'{record_count} total records')
     szumma = all_df[SUM_FIELD].sum()
-    print(f"{DATA_DIR}, {REPORT_MONTH}, total: {szumma:-10,.3f}\n")
+    print(f"{DATA_DIR}, {REPORT_MONTH}, total: {szumma:-10,.2f} | {record_count:10d} total record count\n")
     sqw.write_to_db(all_df, TABLE, db_name='stage', action='replace', field_lens='vchall', hova=hova)
 
 
 def main():
-    scribd('/Users/frank/pd/Nextcloud/szamitas', hova='0')
-    # hoopla('h:/Nextcloud/Finance/szamitas', hova='0')
+    scribd(hova='0')
 
 
 if __name__ == '__main__':
