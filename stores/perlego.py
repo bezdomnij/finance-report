e4856f0ac0866c6624386f4321925d3d7099b3cc
bezdomnij <attlila.novak@yahoo.co.uk>
2022-04-27 22:57:18 +0200

    prepering for march with perlego

--- stores/perlego.py
+++ stores/perlego.py
@@ -1,16 +1,15 @@
 from pathlib import Path
 import pandas as pd
 from engineer import sql_writer as sqw
+from config import MAIN_DIR, REPORT_MONTH
 
 TABLE = 'stg_fin2_37_perlego'
 FILENAME = 'Sales_Report'
-SOURCE_DIR = '2022_02_february'
-REPORT_MONTH = '2022_02_february'
 DATA_DIR = 'perlego'
 SUM_FIELD = 'royalty_share'
 
 
-def perlego(dirpath, hova='0'):
+def perlego(hova='0'):
     """
     All perlego files in source directory back to 19. All assembled together and written once to db
     :param dirpath: source dir, sales report
@@ -18,7 +17,7 @@ def perlego(dirpath, hova='0'):
     :return: nothing, just action
     get the date from the filename - name files properly!
     """
-    p = Path(dirpath).joinpath(SOURCE_DIR).joinpath(DATA_DIR)
+    p = Path(MAIN_DIR).joinpath(REPORT_MONTH).joinpath(DATA_DIR)
     # disable chained assignments
     pd.options.mode.chained_assignment = None
     df_all = pd.DataFrame()
@@ -27,8 +26,8 @@ def perlego(dirpath, hova='0'):
     for f in files:
         if f.suffix == '.csv':  # and FILENAME in f.stem:
             df = pd.read_csv(f, encoding='utf-8', header=1)
-            # df['Date'] = REPORT_MONTH[:4] + '-' + REPORT_MONTH[5:7] + '-15'
-            df['Date'] = f.stem[:4] + '-' + f.stem[4:6] + '-15'
+            df['Date'] = REPORT_MONTH[:4] + '-' + REPORT_MONTH[5:7] + '-15'
+            # df['Date'] = f.stem[:4] + '-' + f.stem[4:6] + '-15'
             df[SUM_FIELD] = df[SUM_FIELD].str.strip().str.slice(start=1)
 
             print(f"{f.stem}\t{round(df[SUM_FIELD].astype('float64').sum(), 2)}\t{df.shape[0]}\trecords")
@@ -42,8 +41,7 @@ def perlego(dirpath, hova='0'):
 
 
 def main():
-    # perlego('/Users/frank/pd/Nextcloud/szamitas', hova='19')
-    perlego('h:/Nextcloud/Finance/szamitas', hova='pd')
+    perlego(hova='pd')
 
 
 if __name__ == '__main__':
