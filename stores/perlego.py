3ef14011ec6b5860eca0c2f79c147c3979fb0ca4
bezdomnij <attlila.novak@yahoo.co.uk>
2022-04-28 08:23:39 +0200

    perlego set date with offset

--- stores/perlego.py
+++ stores/perlego.py
@@ -7,6 +7,17 @@ TABLE = 'stg_fin2_37_perlego'
 FILENAME = 'Sales_Report'
 DATA_DIR = 'perlego'
 SUM_FIELD = 'royalty_share'
+# REPORT_MONTH = '2022_04_april'
+OFFSET = -1
+
+
+def set_date():
+    parts = REPORT_MONTH.split('_')
+    year, month = int(parts[0]), int(parts[1])
+    temp_month = month + OFFSET
+    month = month + OFFSET if temp_month != 0 else 12
+    year = year if temp_month != 0 else year - 1
+    return '_'.join([str(year), str(month).zfill(2)])
 
 
 def perlego(hova='0'):
@@ -17,6 +28,7 @@ def perlego(hova='0'):
     :return: nothing, just action
     get the date from the filename - name files properly!
     """
+    print(REPORT_MONTH)
     p = Path(MAIN_DIR).joinpath(REPORT_MONTH).joinpath(DATA_DIR)
     # disable chained assignments
     pd.options.mode.chained_assignment = None
@@ -26,7 +38,10 @@ def perlego(hova='0'):
     for f in files:
         if f.suffix == '.csv':  # and FILENAME in f.stem:
             df = pd.read_csv(f, encoding='utf-8', header=1)
-            df['Date'] = REPORT_MONTH[:4] + '-' + REPORT_MONTH[5:7] + '-15'
+            base_date = REPORT_MONTH
+            if OFFSET != 0:
+                base_date = set_date()
+            df['Date'] = base_date[:4] + '-' + base_date[5:7] + '-15'
             # df['Date'] = f.stem[:4] + '-' + f.stem[4:6] + '-15'
             df[SUM_FIELD] = df[SUM_FIELD].str.strip().str.slice(start=1)
 
@@ -35,13 +50,13 @@ def perlego(hova='0'):
 
     szumma = df_all[SUM_FIELD].astype('float64').sum()
     print(f"{DATA_DIR}, {REPORT_MONTH}, total: {szumma:-10,.2f}\n")
-    # df_all.info()
+    print(df_all.head())
     df_all = df_all.sort_values(by='Date')
     sqw.write_to_db(df_all, TABLE, hova=hova, action='replace', field_lens='vchall')
 
 
 def main():
-    perlego(hova='pd')
+    perlego(hova='19')
 
 
 if __name__ == '__main__':
