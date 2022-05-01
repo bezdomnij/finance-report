3b330171d63f715ba43dc11ce572e70b4f122793
bezdomnij <attlila.novak@yahoo.co.uk>
2022-05-01 10:43:31 +0200

    libreka reform - able to handle multiple files

--- stores/libreka.py
+++ stores/libreka.py
@@ -17,9 +17,9 @@ def get_content(f, df, s):
     df['month'] = pd.DatetimeIndex(df['Datum']).month
     df.fillna(value='', inplace=True)
     net_income = df[SUM_FIELD].sum()
-    print(f"{s} - {f.stem[-6:]}: {net_income:12,.2f}, {df['Datum'].min()}, "
-          f"{df['Datum'].max()}, - {f.stem[-2:]}, {df.shape[0]}, "
-          f"{df.groupby(['month']).size()}")
+    # print(f"{s} - {f.stem[-6:]}: {net_income:12,.2f}, {df['Datum'].min()}, "
+    #       f"{df['Datum'].max()}, - {f.stem[-2:]}, {df.shape[0]}, "
+    #       f"{df.groupby(['month']).size()}")
     return df, net_income
 
 
@@ -30,33 +30,30 @@ def libreka(hova=HOVA):
         return
     if len(files) > 0:
         sheet_names = ['E-Book-Verkäufe', 'Abo und Flatrate']
-        df_collection = {}
+        df_collection = dict.fromkeys(sheet_names)
+        # init
+        for s in sheet_names:
+            table = TABLE_EBOOK if s == 'E-Book-Verkäufe' else TABLE_SUB
+            df_collection[s] = []
+            df_collection[s].append(table)
+            df_collection[s].append(pd.DataFrame())
+            df_collection[s].append(0)
+
         for f in files:
             if f.suffix == '.xlsx' and '5288812' in f.stem and f.stem[:2] != '~$':
                 df_dict = pd.read_excel(f, sheet_name=None, header=0)
-
-                for s in sheet_names:  # collect dfs and sums
-                    # initialize
-                    table = str(s) + '_table'
-                    df_collection[s] = []  # pd.DataFrame()
-                    df_collection[s].append(pd.DataFrame())
-                    df_collection[s].append(0)
-                    df_collection[s].append(table)
-
-                    # action on this df
+                for s in sheet_names:
                     df = df_dict.get(s, pd.DataFrame())
                     df, szumma = get_content(f, df, s)
-                    df_collection[s][0] = df_collection[s][0].append(df, ignore_index=True)
-                    df_collection[s][1] += szumma
-                    print(f"{s} min.Date: {df_collection[s][0]['Datum'].min()} | "
-                          f"max.Date: {df_collection[s][0]['Datum'].max()}")
+                    df_collection[s][1] = df_collection[s][1].append(df, ignore_index=True)
+                    df_collection[s][2] += szumma
 
-        # for k, v in df_collection.items():
-        #     print(k, v)
-        # sqw.write_to_db(, TABLE_SUB, field_lens='vchall', hova=hova, action='replace')
-        # sqw.write_to_db(libreka_all_ebook, TABLE_EBOOK, field_lens='vchall', hova=hova, action='replace')
-        # print(f"{DATA_DIR.upper()} | ebooks: {ebook_sum:-10,.2f}; subs: {sub_sum:-10,.2f}; "
-        #       f"sumsum: {ebook_sum + sub_sum:-10,.2f}\n")
+                    print(f"{s} min.Date: {df_collection[s][1]['Datum'].min()} | "
+                          f"max.Date: {df_collection[s][1]['Datum'].max()}")
+        for k, v in df_collection.items():
+            # print(k, v)
+            sqw.write_to_db(v[1], v[0], field_lens='vchall', hova=hova, action='replace')
+            print(f"{DATA_DIR.upper()} | {v[0]}, {v[2]}\n")
     else:
         util.empty(DATA_DIR)
 
